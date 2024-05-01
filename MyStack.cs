25ed8c7acb48fb5c2cb3939b3ebb38fd395b54ee
hikmet <hikmet_xidirzada57@mail.ru>
2024-05-01 15:58:35 +0400

    01.05.2024

---
+++ MyStack.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_queue
+{
+    public class MyStack<T> : IEnumerable
+    {
+        private T[] items;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+        private int lastIndex;
+
+        public MyStack()
+        {
+            Capacity = 4;
+            items = new T[Capacity];
+            Count = 0;
+            lastIndex = -1;
+        }
+
+        public MyStack(int capacity)
+        {
+            if (capacity >= 0)
+            {
+                Capacity = capacity;
+                items = new T[Capacity];
+                Count = 0;
+                lastIndex = -1;
+            }
+            else
+            {
+                throw new InvalidOperationException("Capacity cannot be less than 0");
+            }
+        }
+
+        public void Push(T item)
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2;
+                Array.Resize(ref items, Capacity);
+            }
+
+            lastIndex++;
+            items[lastIndex] = item;
+            Count++;
+        }
+
+        public T Peek()
+        {
+            if (lastIndex == -1)
+            {
+                throw new InvalidOperationException("Empty");
+            }
+
+            return items[lastIndex];
+        }
+
+        public T Pop()
+        {
+            if (lastIndex == -1)
+            {
+                throw new InvalidOperationException("Empty list");
+            }
+
+            T removedElement = items[lastIndex];
+            lastIndex--;
+            Count--;
+
+            if (Count < Capacity / 4)
+            {
+                Capacity /= 2;
+                Array.Resize(ref items, Capacity);
+            }
+            return removedElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = lastIndex; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Stack_and_queue;
+
+MyStack<string> test =new MyStack<string>();
+
+test.Push("s");
+test.Push("d");
+test.Push("g");
+test.Push("f");
+Console.WriteLine("\n");
+Console.WriteLine("Before methods : \n");
+foreach (var item in test)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Popped element: {test.Pop()} \n");
+
+Console.WriteLine("List of the elements :");
+foreach (var item in test)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Peeked element: {test.Peek()} \n");
+
+Console.WriteLine($"Capacity of list : {test.Capacity}");
+Console.WriteLine($"Count of elements: {test.Count}");
